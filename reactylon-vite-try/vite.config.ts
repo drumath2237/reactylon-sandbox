8fad1e0fecb95bef351a025cb346a99c284fa63f
drumath2237 <2237k.drumath@gmail.com>
2025-11-29 21:31:31 +0900

    :green_heart: fix for gh action

--- reactylon-vite-try/vite.config.ts
+++ reactylon-vite-try/vite.config.ts
@@ -2,15 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(
-    {
-      babel: {
-        plugins: [
-          "babel-plugin-react-compiler",
-          "babel-plugin-reactylon",
-        ],
+export default defineConfig(({ mode }) => {
+  return {
+    base: mode === "production" ? "/reactylon-sandbox/" : "",
+    plugins: [react(
+      {
+        babel: {
+          plugins: [
+            "babel-plugin-react-compiler",
+            "babel-plugin-reactylon",
+          ],
+        },
       },
-    },
-  )],
+    )],
+  };
 });
