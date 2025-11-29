cad18ab7edef732680d08abdad5a5918eb057e22
drumath2237 <2237k.drumath@gmail.com>
2025-11-29 20:55:21 +0900

    :sparkles: 修正

--- reactylon-vite-try/src/components/Content.tsx
+++ reactylon-vite-try/src/components/Content.tsx
@@ -1,17 +1,23 @@
-import { MeshBuilder, Vector3, } from "@babylonjs/core";
+import { MeshBuilder, Vector3 } from "@babylonjs/core";
 import { useEffect } from "react";
 import { useScene } from "reactylon";
 
 export function Content() {
-  const scene = useScene()
+  const scene = useScene();
 
   useEffect(() => {
-    const box = MeshBuilder.CreateBox("box", { size: 0.2 })
+    const box = MeshBuilder.CreateBox("box", { size: 0.2 });
 
-    scene.registerBeforeRender(() => {
-      box.rotate(Vector3.Up(), 0.01)
-    })
+    const onBeforeRender = () => {
+      box.rotate(Vector3.Up(), 0.01);
+    };
+    scene.registerBeforeRender(onBeforeRender);
+
+    return () => {
+      scene.unregisterBeforeRender(onBeforeRender);
+      box.dispose();
+    };
   });
 
-  return <></>
+  return <></>;
 }
