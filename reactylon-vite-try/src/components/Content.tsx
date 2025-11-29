c3645bb2a651e5beedbebae2e533f0bc816eb518
drumath2237 <2237k.drumath@gmail.com>
2025-11-29 21:14:56 +0900

    :bug: これだとなぜか動かない

--- reactylon-vite-try/src/components/Content.tsx
+++ reactylon-vite-try/src/components/Content.tsx
@@ -1,23 +1,5 @@
-import { MeshBuilder, Vector3 } from "@babylonjs/core";
-import { useEffect } from "react";
-import { useScene } from "reactylon";
-
 export function Content() {
-  const scene = useScene();
-
-  useEffect(() => {
-    const box = MeshBuilder.CreateBox("box", { size: 0.2 });
-
-    const onBeforeRender = () => {
-      box.rotate(Vector3.Up(), 0.01);
-    };
-    scene.registerBeforeRender(onBeforeRender);
-
-    return () => {
-      scene.unregisterBeforeRender(onBeforeRender);
-      box.dispose();
-    };
-  });
-
-  return <></>;
+  return (
+    <box name="box" options={{ size: 0.2 }} />
+  )
 }
